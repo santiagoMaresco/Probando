c03e7c6fb0dda735e4809674027b7d6f2e6b7fbb
marcofbb <marco.fbb@gmail.com>
2016-04-26 20:33:52 -0300

    Agregado lo que faltaba

--- src/test/java/Poi.java
+++ src/test/java/Poi.java
@@ -1,26 +1,75 @@
-
 public class Poi {
-	private double latitud;
-	private double longitud;
-	private String nombre;
-	private String queSoy;
+	public double latitud;
+	public double longitud;
+	public String nombre;
+	public String queSoy;
+	
+	/*
+	 * INITIALIZE
+	 */
+	
+	public Poi(){
+		this.latitud = 0;
+		this.longitud = 0;
+		this.nombre = null;
+		this.queSoy = null;
+	}
+	
+	/*
+	 * SETs y GETs
+	 */
+	
+	public void setNombre(String nom) {
+		this.nombre = nom;
+	}
 	
 	public void setGeolocalizar(double unaLati, double unaLongi) {
 		this.latitud = unaLati;
 		this.longitud = unaLongi;
 	}
-
-	public boolean esValido() {
-		if(this.latitud == 0 || this.longitud == 0){
-			return false;
-		}else{
-		
-		return true;
-		}
-		}
-
-	public void setNombre(String nom) {
-		this.nombre = nom;
+	
+	/*
+	 * toRad();
+	 * Detalle: Convierto a radianes
+	 */
+	private double toRad(double value) {
+	    return value * Math.PI / 180;
 	}
-
+	
+	/*
+	 * distanciaA();
+	 * Detalle: Devuelve la distancia que hay hasta unPoi, en metros.
+	 */
+	public double distanciaA(Poi unPoi){
+		double radio_tierra = 6378.137; // Definido por WGS84
+		double diferencia_longitud = unPoi.longitud - this.longitud;
+		double diferencia_latitud = unPoi.latitud - this.latitud;
+		
+		// Formula matematica
+		double a = Math.sin(diferencia_latitud/2) * Math.sin(diferencia_latitud/2) + Math.cos(toRad(this.latitud)) * Math.cos(toRad(unPoi.latitud)) * Math.sin(diferencia_longitud/2) * Math.sin(diferencia_longitud/2);
+		double b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		double c = (radio_tierra * b);
+		//
+		return (c*1000); // Lo convierto en metros
+	}
+	
+	/*
+	 * dentroRadio();
+	 * Detalle: Compara la distancia entre los POI, contra un radio en metros.
+	 */
+	public boolean dentroRadio(Poi unPoi, double radio){
+		if(distanciaA(unPoi) > radio) return false;
+		return true;
+	}
+	
+	
+	/*
+	 * esValido();
+	 * Detalle: Debe tener latitud, longitud y nombre
+	 */
+	public boolean esValido() {
+		if(this.latitud == 0 || this.longitud == 0 || this.nombre == null) return false;
+		return true;
+	}
+	
 }
