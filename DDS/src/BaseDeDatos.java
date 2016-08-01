abcb7b0f1dc9065d8879509938e470e0fc5f3521
marcofbb <marco.fbb@gmail.com>
2016-08-01 20:10:38 -0300

    Update
    Update

--- DDS/src/BaseDeDatos.java
+++ DDS/src/BaseDeDatos.java
@@ -1,6 +1,8 @@
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.*;
 
 
 public class BaseDeDatos {
@@ -14,7 +16,7 @@ public class BaseDeDatos {
 		registroDeBusqueda.fecha = fecha;
 		registroDeBusqueda.terminal = terminal;
 		registrosDeBusqueda.add(registroDeBusqueda);
-		}
+	}
 	
 	public int cantidadDeBusquedasSegun(String texto)
 	{
@@ -31,6 +33,67 @@ public class BaseDeDatos {
 		return cantidadDeBusquedas;
 	}
 	
+	public void cantidadDeBusquedasPorFecha(){
+		Map<String, Integer> aMap = new HashMap<String, Integer>();
+		int contador_tmp = 0;
+		
+		Iterator<RegistroDeBusqueda> it = (this.registrosDeBusqueda).iterator();
+		while(it.hasNext()){
+			contador_tmp = 0;
+			RegistroDeBusqueda i = it.next();
+			if(aMap.containsKey(i.fecha)){
+				contador_tmp = aMap.get(i.fecha);
+				contador_tmp++;
+				aMap.put(i.fecha, contador_tmp);
+			} else {
+				contador_tmp = 1;
+				aMap.put(i.fecha, contador_tmp);
+			}
+		}
+		
+		//
+		
+		aMap.forEach((k,v) -> System.out.println("Fecha: " + k + " Cantidad de busquedas: " + v));
 	}
+	
+	public void cantidadBusquedasPorFraseYTerminal(){
+		Map<String, Map<String,Integer>> aMap = new HashMap<String, Map<String,Integer>>();
+		Map<String, Integer> aMap2;
+		int contador_tmp = 0;
+		
+		Iterator<RegistroDeBusqueda> it = (this.registrosDeBusqueda).iterator();
+		while(it.hasNext()){
+			contador_tmp = 0;
+			RegistroDeBusqueda i = it.next();
+			if(aMap.containsKey(i.fraseBuscada)){
+				aMap2 = aMap.get(i.fraseBuscada);
+				if(aMap2.containsKey(i.terminal)){
+					contador_tmp = aMap2.get(i.terminal);
+					contador_tmp++;
+					aMap2.put(i.terminal, contador_tmp);
+					aMap.put(i.fraseBuscada, aMap2);
+				} else {
+					contador_tmp = 1;
+					aMap2.put(i.terminal, contador_tmp);
+					aMap.put(i.fraseBuscada, aMap2);
+				}
+			} else {
+				contador_tmp = 1;
+				aMap2 = new HashMap<String, Integer>();
+				aMap2.put(i.terminal, contador_tmp);
+				aMap.put(i.fraseBuscada, aMap2);
+			}
+		}
+		
+		//
+		for (Map.Entry<String, Map<String,Integer>> entry : aMap.entrySet()) {
+			System.out.println("Frase : " + entry.getKey());
+			for (Map.Entry<String, Integer> entry2 : (entry.getValue()).entrySet()) {
+				System.out.println("--> Terminal: " + entry2.getKey() + " Cantidad : " + entry2.getValue());
+			}
+		}
+	}
+	
+}
 	
 
