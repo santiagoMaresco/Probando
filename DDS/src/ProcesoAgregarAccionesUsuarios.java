238bbed3977f397a73cfd5d9d080f902cf7a05c5
mkumert <mati.kumert@gmail.com>
2016-08-02 18:59:58 -0300

    Agrego ejecutar, resultado y deshacer
    Falta registrar las fechas en los resultados!

--- DDS/src/ProcesoAgregarAccionesUsuarios.java
+++ DDS/src/ProcesoAgregarAccionesUsuarios.java
@@ -4,17 +4,42 @@ import java.util.HashSet;
 public class ProcesoAgregarAccionesUsuarios implements Proceso {
 
 	public Collection<ResultadoProceso> resultados = new HashSet<ResultadoProceso>();
+	public Usuario user;
+	public Permisos nuevoPermiso;
+	
+	public void setUser (Usuario unUser){
+		this.user = unUser;
+	}
+	
+	public void determinarPermiso(Permisos unPermiso){
+		this.nuevoPermiso = unPermiso;
+	}
 	
 	@Override
 	public void ejecutar() {
-		// TODO Auto-generated method stub
-
+		user.permisos.add(nuevoPermiso);
+		resultado();
 	}
 
 	@Override
 	public void resultado() {
-		// TODO Auto-generated method stub
+		if(user.permisos.contains(nuevoPermiso)){
+			ResultadoProceso res = new ResultadoProceso();
+			res.procesoEjecutado = this;
+			res.resultado = true;
+		}
+		else{
+			ResultadoProceso res = new ResultadoProceso();
+			res.procesoEjecutado = this;
+			res.resultado = false;
+		}
 
+	}
+	
+	public void deshacer(){ 
+		if(user.permisos.contains(nuevoPermiso)){
+			user.permisos.remove(nuevoPermiso);
+		}
 	}
 
 }
